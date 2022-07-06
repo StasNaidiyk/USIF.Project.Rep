36c76dcd41b4a21097f44df367c84e858ee4f810
Stanislav Naidiyk <stas.naidiyk@gmail.com>
2022-07-07 02:39:16 +0300

    Done script_6_2.ps

--- Lesson6/script_6_2.sh
+++ Lesson6/script_6_2.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 # скрипт,  который использовал этот текстовый файл и выводил количество строк. Затем брал бы первые 10 результатов и обрезал путь к файлу по символу “/”, оставляя список только имен файлов. Затем перенаправлял в новый файл > cmd_names.
-cat -b usr_executables.txt > cmd_names
+File=usr_executables.txt
+Result=cmd_names
+wc -l $File > $Result
+cat -b $File >> $Result
+echo 'Назви перших 10 файлів:' >> $Result
+for var in $(head $File) 
+do
+echo ${var##*/} >> $Result
+done
