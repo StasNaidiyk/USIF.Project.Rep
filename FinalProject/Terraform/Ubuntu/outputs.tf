f13a95c6372122a4c1f4be5e35518987cffed702
Stanislav Naidiyk <stas.naidiyk@gmail.com>
2022-10-05 01:23:39 +0300

    1

---
+++ FinalProject/Terraform/Ubuntu/outputs.tf
@@ -0,0 +1,12 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.naidiyk-terraform.name
+}
+
+output "public_ip_address" {
+  value = azurerm_linux_virtual_machine.naidiyk_terraform_ubuntu.public_ip_address
+}
+
+output "tls_private_key" {
+  value     = tls_private_key.example_ssh.private_key_pem
+  sensitive = true
+}
